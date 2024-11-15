eaf1d229ef53098d4feb9ebd2ce9387f359d22d3
yezixx <skylow917@daum.net>
2024-11-15 22:36:57 +0900

    Main class 작성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import menu.Join;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input;
+
+        while(true) {
+            System.out.println("=========================================");
+            System.out.println("메뉴를 선택해주세요.");
+            System.out.println("-----------------------------------------");
+            System.out.println("1. 회원가입");
+            System.out.println("2. 로그인");
+            System.out.println("3. 종료");
+            System.out.println("=========================================");
+            input = br.readLine();
+            switch (input){
+                case "1":
+                    Join join = new Join();
+                    join.join();
+                    break;
+                case "2":
+                    System.out.println("로그인 선택");
+                    break;
+                case "3":
+                    System.out.println("=========================================");
+                    System.out.println("프로그램을 종료합니다.");
+                    System.out.println("=========================================");
+                    return;
+                default:
+                    System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+                    System.out.println("존재하지 않는 메뉴입니다. 다시 선택해주세요.");
+                    System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+            }
+        }
     }
 }
