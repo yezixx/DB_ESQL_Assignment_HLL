271e18f29167791dcd8495a9d9a05924bd42bee0
yezixx <skylow917@daum.net>
2024-11-20 17:09:36 +0900

    불필요한 import문 제거

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-import config.DBConfig;
-import menu.Join;
-import menu.Login;
 import menu.MainMenu;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws IOException {
